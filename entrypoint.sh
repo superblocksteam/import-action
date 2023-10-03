c8838fac6019f9a686702ced5988f9ec6fd3cde2
Taha Saifuddin <tahashabbir27@gmail.com>
2023-10-03 17:11:07 -0400

    Fix shellcheck issues

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
 SHA="$1"
 TOKEN="$2"
@@ -9,25 +9,29 @@ GITHUB_TOKEN="$6"
 
 SUPERBLOCKS_BOT_NAME="superblocks-app[bot]"
 
-cd $GITHUB_WORKSPACE
+echo "#### Printing out the usr binaries"
+ls -l /usr/bin
+
+
+cd "$GITHUB_WORKSPACE" || exit 1
 git config --global --add safe.directory "$GITHUB_WORKSPACE"
 
 # Check if the commit in question was made by Superblocks
 actor_name=$(git show -s --format='%an' "$SHA")
-if [ "$actor_name" != "$SUPERBLOCKS_BOT_NAME" ]; then
+if [ "$actor_name" == "$SUPERBLOCKS_BOT_NAME" ]; then
     echo "Commit was made by Superblocks. Skipping push..."
     exit 0
 fi
 
 # Get the list of changed files in the last commit
-changed_files=$(git diff ${SHA}^ --name-only)
+changed_files=$(git diff "${SHA}"^ --name-only)
 
 if [ -n "$changed_files" ]; then
     echo "Files changed since the last commit:"
     echo "$changed_files"
 
     # TODO(taha) Remove the following once the push-compatible version of the CLI is released
-    npm set //npm.pkg.github.com/:_authToken ${{ inputs.github-npm-read-token }}
+    npm set //npm.pkg.github.com/:_authToken "$GITHUB_TOKEN"
     echo "@superblocksteam:registry=https://npm.pkg.github.com/" >> ~/.npmrc
 
     # Install Superblocks CLI
@@ -48,7 +52,7 @@ folder_changed() {
 }
 
 # Read superblocks config config file json
-json_data=$(cat $PATH)
+json_data=$(cat "$PATH")
 
 # Loop through folder paths and check if they have changed
 jq -r '.resources[].location' <<< "$json_data" | while IFS= read -r folder_path; do
@@ -57,4 +61,4 @@ jq -r '.resources[].location' <<< "$json_data" | while IFS= read -r folder_path;
 done
 
 # use if we need to output later
-echo "imported=COMPLETED" >> $GITHUB_OUTPUT
+echo "imported=COMPLETED" >> "$GITHUB_OUTPUT"
