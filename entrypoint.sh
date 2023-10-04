5c9bc0aef824ee2b3006eb0912aade58a157036e
Taha Saifuddin <tahashabbir27@gmail.com>
2023-10-04 00:39:10 -0400

    Remove some debugging statements

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+set -e
+set -o pipefail
+
 SHA="$1"
 TOKEN="$2"
 DOMAIN="$3"
@@ -9,13 +12,13 @@ GITHUB_TOKEN="$6"
 
 SUPERBLOCKS_BOT_NAME="superblocks-app[bot]"
 
-cd "$GITHUB_WORKSPACE" || exit 1
+cd "$GITHUB_WORKSPACE"
 git config --global --add safe.directory "$GITHUB_WORKSPACE"
 
-# Check if the commit in question was made by Superblocks
+# Get the name of the actor who made the last commit
 actor_name=$(git show -s --format='%an' "$SHA")
 if [ "$actor_name" == "$SUPERBLOCKS_BOT_NAME" ]; then
-    echo "Commit was made by Superblocks. Skipping push..."
+    printf "\nCommit was made by Superblocks. Skipping push...\n"
     exit 0
 fi
 
@@ -23,14 +26,12 @@ fi
 changed_files=$(git diff "${SHA}"^ --name-only)
 
 if [ -n "$changed_files" ]; then
-    echo "Files changed since the last commit:"
-    echo "$changed_files"
-
     # TODO(taha) Remove the following once the push-compatible version of the CLI is released
     npm set //npm.pkg.github.com/:_authToken "$GITHUB_TOKEN"
     echo "@superblocksteam:registry=https://npm.pkg.github.com/" >> ~/.npmrc
 
     # Install Superblocks CLI
+    printf "\nInstalling Superblocks CLI ($CLI_VERSION)...\n"
     npm install -g @superblocksteam/cli@"$CLI_VERSION"
 
     # Login to Superblocks
@@ -45,7 +46,7 @@ fi
 folder_changed() {
     local folder_path="$1"
     if echo "$changed_files" | grep -q "^$folder_path/"; then
-        echo "Folder '$folder_path' has changed in the last commit. Pushing..."
+        printf "\nChange detected. Pushing...\n"
         superblocks push "$folder_path"
     fi
 }
@@ -55,9 +56,6 @@ json_data=$(cat "$CONFIG_PATH")
 
 # Loop through folder paths and check if they have changed
 jq -r '.resources[].location' <<< "$json_data" | while IFS= read -r folder_path; do
-    echo "Checking $folder_path"
+    printf "\nChecking $folder_path for changes...\n"
     folder_changed "$folder_path"
 done
-
-# use if we need to output later
-echo "imported=COMPLETED" >> "$GITHUB_OUTPUT"
