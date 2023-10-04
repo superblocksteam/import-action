fc39e83addb210fcbb131915bf70ccd6343168be
Taha Saifuddin <50331398+taha-au@users.noreply.github.com>
2023-10-04 01:41:55 -0400

    Ensure that push is only attempted for applications (#5)
    * Remove bin printing and skip push if no files changed
    
    * Remove some debugging statements
    
    * Filter out all non-application resources

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+set -e
+set -o pipefail
+
 SHA="$1"
 TOKEN="$2"
 DOMAIN="$3"
@@ -9,17 +12,13 @@ GITHUB_TOKEN="$6"
 
 SUPERBLOCKS_BOT_NAME="superblocks-app[bot]"
 
-echo "#### Printing out the usr binaries"
-ls -l /usr/bin
-
-
-cd "$GITHUB_WORKSPACE" || exit 1
+cd "$GITHUB_WORKSPACE"
 git config --global --add safe.directory "$GITHUB_WORKSPACE"
 
-# Check if the commit in question was made by Superblocks
+# Get the name of the actor who made the last commit
 actor_name=$(git show -s --format='%an' "$SHA")
 if [ "$actor_name" == "$SUPERBLOCKS_BOT_NAME" ]; then
-    echo "Commit was made by Superblocks. Skipping push..."
+    printf "\nCommit was made by Superblocks. Skipping push...\n"
     exit 0
 fi
 
@@ -27,38 +26,35 @@ fi
 changed_files=$(git diff "${SHA}"^ --name-only)
 
 if [ -n "$changed_files" ]; then
-    echo "Files changed since the last commit:"
-    echo "$changed_files"
-
     # TODO(taha) Remove the following once the push-compatible version of the CLI is released
     npm set //npm.pkg.github.com/:_authToken "$GITHUB_TOKEN"
     echo "@superblocksteam:registry=https://npm.pkg.github.com/" >> ~/.npmrc
 
     # Install Superblocks CLI
+    printf "\nInstalling Superblocks CLI (%s)...\n" "$CLI_VERSION"
     npm install -g @superblocksteam/cli@"$CLI_VERSION"
+    superblocks --version
 
     # Login to Superblocks
+    printf "\nLogging in to Superblocks...\n"
     superblocks config set domain "$DOMAIN"
     superblocks login -t "$TOKEN"
+else
+    echo "No files changed since the last commit. Skipping push..."
+    exit 0
 fi
 
-# Function to check if a folder path is in the list of changed files
-folder_changed() {
-    local folder_path="$1"
-    if echo "$changed_files" | grep -q "^$folder_path"; then
-        echo "Folder '$folder_path' has changed in the last commit. Pushing..."
-        superblocks push "$folder_path"
+# Function to push a resource to Superblocks if it has changed
+push_resource() {
+    local location="$1"
+    if echo "$changed_files" | grep -q "^$location/"; then
+        printf "\nChange detected. Pushing...\n"
+        superblocks push "$location"
     fi
 }
 
-# Read superblocks config config file json
-json_data=$(cat "$CONFIG_PATH")
-
-# Loop through folder paths and check if they have changed
-jq -r '.resources[].location' <<< "$json_data" | while IFS= read -r folder_path; do
-    echo "Checking $folder_path"
-    folder_changed "$folder_path"
+# Check if any push-compatible resources have changed
+jq -r '.resources[] | select(.resourceType == "APPLICATION") | .location' "$CONFIG_PATH" | while read -r location; do
+    printf "\nChecking %s for changes...\n" "$location"
+    push_resource "$location"
 done
-
-# use if we need to output later
-echo "imported=COMPLETED" >> "$GITHUB_OUTPUT"
